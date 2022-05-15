1192a4e7d509717dd4a7f268b01c7752fe38cf19
Bea <beatrice.piras99@gmail.com>
2022-05-15 16:47:50 +0200

    Sku some modifications

--- code/server/API/SKU_API.js
+++ code/server/API/SKU_API.js
@@ -107,9 +107,7 @@ app.get('/api/skus/:id', async (req, res) => {
         return res.status(422).json({ error: 'Invalid data' });
     }
 
-    if (s.findSKUbyID(db, id) === 0) { // check sku id existence
-        return res.status(404).json({error: 'Sku ID not found in database'})
-    } 
+   
 
     try {
         const skuFound = await s.getSKUbyID(db, id);
@@ -139,19 +137,20 @@ app.put('/api/sku/:id', async (req, res) => {
     if (Object.keys(req.body).length === 0 || sku === undefined || sku.newDescription === undefined || sku.newWeight === undefined || sku.newVolume === undefined || sku.newPrice === undefined || sku.newAvailableQuantity === undefined ) {
         return res.status(422).json({ error: 'Unprocessable entity' });
     }
-    if (s.findSKUbyID(db, id) === 0) { // check sku id existence
-        return res.status(404).json({error: 'Sku ID not found in database'})
-    }
+    
 
     try {
         await s.updateSKU(db, id, sku);
         return res.status(200).end();
     }
     catch (err) {
-         if (err.message === "Maximum position capacity exceeded") {
+        if (err.message === "ID not found"){
+            res.status(404).end()
+        } else if (err.message === "ID position not found"){
+            res.status(404).end()
+        }else if (err.message === "Maximum position capacity exceeded") {
             res.status(422).json({error: 'Maximum position capacity exceeded'});
         } else {
-            console.log(err);
             res.status(503).json({error: 'Generic error'});
         }
     }
@@ -170,12 +169,6 @@ app.put('/api/sku/:id', async (req, res) => {
         return res.status(422).json({ error: 'Unprocessable entity' });
     }
 
-    if (s.findSKUbyID(db, id) === 0) { // check sku id existence
-        return res.status(404).json({error: 'Sku ID not found in database'});
-    }
-    if(s.findPosbyID(db, position) === 0){ // check position id existence
-        return res.status(404).json({error: 'Position ID not found in database'});
-    }
 
     
 
@@ -185,10 +178,15 @@ app.put('/api/sku/:id', async (req, res) => {
         return res.status(200).end();
     }
     catch (err) {
-        if (err.message === "Maximum position capacity exceeded") {
+        if (err.message === "ID not found"){
+            res.status(404).end()
+        } else if (err.message === "ID position not found"){
+            res.status(404).end()
+        }
+        else if (err.message === "Maximum position capacity exceeded") {
             res.status(422).json({error: 'Maximum position capacity exceeded'});
         } else {
-            res.status(503).json({error: 'Generic error'});
+            res.status(503).end();
         }
     }
 
