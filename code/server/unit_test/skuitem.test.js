b328efb79e5d57f0abd968c5db409190b0024f79
Lorenzo Sciara <s303462@studenti.polito.it>
2022-05-17 19:03:47 +0200

    sku test update

--- code/server/unit_test/skuitem.test.js
+++ code/server/unit_test/skuitem.test.js
@@ -18,13 +18,15 @@ describe("get skuitems", () => {
             console.log(err)
         }
     })
-    testSkuitem("12345678901234567890123456789041", 3, "2021/11/29 12:30")
+    testSkuitem("12345678901234567890123456789041", 3, "2021/11/29 12:30");
+    //testSkuItem("12345678901234567890123456789041", 3, "2021/11/29 12:30");
+    testStoreSKUItem("12345678901234567890123456789014", 1, "2021/11/29 12:30");
 });
 
 
 function testSkuitem(id, skuid, dateOfStock){
     describe("get skuitem list", () => {
-        test("get skuitem", async () => {
+        test("test of get stored SKUItem", async () => {
             let res = await s.getStoredSKUItem(db)
             expect(res).toEqual([{
                 RFID:id,
@@ -34,4 +36,25 @@ function testSkuitem(id, skuid, dateOfStock){
             }])
         })
     })
+}
+
+function testStoreSKUItem(rfid, skuItem, available, dateOfStock) {
+    test('Store new SKU Item', async () => {
+        
+        const data ={
+            RFID: rfid,
+            SKUItem: skuItem,
+            Available: available,
+            DateOfStock: dateOfStock,
+        }
+        await s.storeSKUItem(db, data);
+        
+        var res = await s.getStoredSKUItemByRFID(db, rfid);
+        expect(res.length).toStrictEqual(1);
+
+        expect(res.RFID).toStrictEqual(data.RFID);
+        expect(res.SKUId).toStrictEqual(data.SKUId);
+        expect(res.Available).toStrictEqual(data.Available);
+        expect(res.DateOfStock).toStrictEqual(data.DateOfStock);
+    });
 }
