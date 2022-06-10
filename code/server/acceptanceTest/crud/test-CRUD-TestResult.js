eaba12724e3dc8563a0d77150955c7e0b31f5bf8
Luca Ardito <luca.ardito@polito.it>
2022-06-10 10:44:40 +0200

    restore acceptance tests

--- code/server/acceptanceTest/crud/test-CRUD-TestResult.js
+++ code/server/acceptanceTest/crud/test-CRUD-TestResult.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 chai.should();
-const {app} = require('../../server'); 
+const app = require('../../server'); 
 var agent = chai.request.agent(app);
 
 const skus = require('../utils-sku');
